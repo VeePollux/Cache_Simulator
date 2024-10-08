bc778972bb695d2db20245de2c55d4c9d9e307b8
VeePollux <aldrighivanessa@gmail.com>
2024-10-08 19:22:18 -0300

    Atualização

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,4 @@
 module com.cache_simulator {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
 
-
-    opens com.cache_simulator to javafx.fxml;
     exports com.cache_simulator;
 }
